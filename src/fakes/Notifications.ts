ec7b0fe21ada023db88ac2260b78507485ec6218
Vladimir Iakovlev <nvbn.rm@gmail.com>
2021-01-23 15:55:43 +0100

    #N/A: Add a fake for INotifications

---
+++ src/fakes/Notifications.ts
@@ -0,0 +1,46 @@
+import INotifications, {
+  NotificationPermissions,
+  NotificationPushToke,
+  NOTIFICATIONS_PERMISSIONS_GRANTED,
+} from "../dependencies/INotifications";
+
+type Behavior = {
+  scheduleNotificationAsync: Promise<string>;
+  getPermissionsAsync: Promise<NotificationPermissions>;
+  requestPermissionsAsync: Promise<NotificationPermissions>;
+  getExpoPushTokenAsync: Promise<NotificationPushToke>;
+  setNotificationChannelAsync: Promise<void>;
+};
+
+export const NOTIFICATIONS_DEFAULT_FAKE_TOKEN = "EXPO_PUSH_NOTIFICATIONS_TOKEN";
+
+const defaultBehavior: Behavior = {
+  scheduleNotificationAsync: Promise.resolve("ok"),
+  getPermissionsAsync: Promise.resolve({
+    status: NOTIFICATIONS_PERMISSIONS_GRANTED,
+  }),
+  requestPermissionsAsync: Promise.resolve({
+    status: NOTIFICATIONS_PERMISSIONS_GRANTED,
+  }),
+  getExpoPushTokenAsync: Promise.resolve({
+    data: NOTIFICATIONS_DEFAULT_FAKE_TOKEN,
+  }),
+  setNotificationChannelAsync: Promise.resolve(),
+};
+
+/**
+ * Create a fake for INotifications that implements provided behavior
+ */
+export const makeNotificationsWithBehavior = (
+  behaviorOverride: Partial<Behavior> = {},
+): INotifications => {
+  const behavior = { ...defaultBehavior, behaviorOverride };
+
+  return {
+    scheduleNotificationAsync: () => behavior.scheduleNotificationAsync,
+    getPermissionsAsync: () => behavior.getPermissionsAsync,
+    requestPermissionsAsync: () => behavior.requestPermissionsAsync,
+    getExpoPushTokenAsync: () => behavior.getExpoPushTokenAsync,
+    setNotificationChannelAsync: () => behavior.setNotificationChannelAsync,
+  };
+};
--- src/utils/__tests__/notifications.test.ts
+++ src/utils/__tests__/notifications.test.ts
@@ -5,13 +5,22 @@ import {
   REGISTER_OK,
   registerForPushNotifications,
 } from "../notifications";
+import {
+  makeNotificationsWithBehavior,
+  NOTIFICATIONS_DEFAULT_FAKE_TOKEN,
+} from "../../fakes/Notifications";
+import {
+  NOTIFICATION_PERMISSIONS_DENIED,
+  NOTIFICATIONS_PERMISSIONS_GRANTED,
+  NOTIFICATIONS_PERMISSIONS_UNDETERMINED,
+} from "../../dependencies/INotifications";
 
 describe("registerForPushNotifications", () => {
   test("can't register if it's not a real device", async () => {
     const { status } = await registerForPushNotifications(
       { isDevice: false },
       {} as any,
-      {} as any,
+      makeNotificationsWithBehavior(),
       false,
     );
 
@@ -19,36 +28,33 @@ describe("registerForPushNotifications", () => {
   });
 
   test("requests permissions the first time", async () => {
-    const notificationsMock = {
-      getPermissionsAsync: jest
-        .fn()
-        .mockReturnValue({ status: "undetermined" }),
-      requestPermissionsAsync: jest.fn().mockReturnValue({ status: "granted" }),
-      getExpoPushTokenAsync: jest.fn().mockReturnValue({ data: "token" }),
-    };
-
     const { status, token } = await registerForPushNotifications(
       { isDevice: true },
       {} as any,
-      notificationsMock as any,
+      makeNotificationsWithBehavior({
+        getPermissionsAsync: Promise.resolve({
+          status: NOTIFICATIONS_PERMISSIONS_UNDETERMINED,
+        }),
+        requestPermissionsAsync: Promise.resolve({
+          status: NOTIFICATIONS_PERMISSIONS_GRANTED,
+        }),
+      }),
       false,
     );
 
     expect(status).toBe(REGISTER_OK);
-    expect(token).toEqual("token");
+    expect(token).toEqual(NOTIFICATIONS_DEFAULT_FAKE_TOKEN);
   });
 
   test("don't re-request permissions if not asked", async () => {
-    const notificationsMock = {
-      getPermissionsAsync: jest.fn().mockReturnValue({ status: "denied" }),
-      requestPermissionsAsync: jest.fn().mockReturnValue({}),
-      getExpoPushTokenAsync: jest.fn().mockReturnValue({}),
-    };
-
     const { status } = await registerForPushNotifications(
       { isDevice: true },
       {} as any,
-      notificationsMock as any,
+      makeNotificationsWithBehavior({
+        getPermissionsAsync: Promise.resolve({
+          status: NOTIFICATION_PERMISSIONS_DENIED,
+        }),
+      }),
       false,
     );
 
@@ -56,20 +62,21 @@ describe("registerForPushNotifications", () => {
   });
 
   test("re-request permissions if asked", async () => {
-    const notificationsMock = {
-      getPermissionsAsync: jest.fn().mockReturnValue({ status: "denied" }),
-      requestPermissionsAsync: jest.fn().mockReturnValue({ status: "granted" }),
-      getExpoPushTokenAsync: jest.fn().mockReturnValue({ data: "token" }),
-    };
-
     const { status, token } = await registerForPushNotifications(
       { isDevice: true },
       {} as any,
-      notificationsMock as any,
+      makeNotificationsWithBehavior({
+        getPermissionsAsync: Promise.resolve({
+          status: NOTIFICATION_PERMISSIONS_DENIED,
+        }),
+        requestPermissionsAsync: Promise.resolve({
+          status: NOTIFICATIONS_PERMISSIONS_GRANTED,
+        }),
+      }),
       true,
     );
 
     expect(status).toBe(REGISTER_OK);
-    expect(token).toEqual("token");
+    expect(token).toEqual(NOTIFICATIONS_DEFAULT_FAKE_TOKEN);
   });
 });
