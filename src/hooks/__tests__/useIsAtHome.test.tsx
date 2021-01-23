ba746c1048f2fabf34d1887dbbcecd62dcea358a
Vladimir Iakovlev <nvbn.rm@gmail.com>
2021-01-23 15:38:15 +0100

    #N/A: Aff a fake for INetwork

--- src/hooks/__tests__/useIsAtHome.test.tsx
+++ src/hooks/__tests__/useIsAtHome.test.tsx
@@ -1,23 +1,23 @@
 /**
  * @jest-environment jsdom
  */
-/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-function-return-type */
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { renderHook } from "@testing-library/react-hooks";
 import Network from "../../contexts/Network";
-import { NETWORK_TYPE_WIFI } from "../../dependencies/INetwork";
-import useIsAtHome from "../useIsAtHome";
 import { isReady } from "../../utils/future";
+import {
+  makeNetworkAsAFailure,
+  makeNetworkAsNotConnected,
+  makeNetworkAsOnWifi,
+} from "../../fakes/Network";
+import useIsAtHome from "../useIsAtHome";
 
 describe("useIsAtHome", () => {
   test("is at home when on wifi", async () => {
-    const network = {
-      async getNetworkStateAsync(): Promise<any> {
-        return { type: NETWORK_TYPE_WIFI };
-      },
-    };
-    const wrapper = ({ children }: any) => (
-      <Network.Provider value={network}>{children}</Network.Provider>
+    const wrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => (
+      <Network.Provider value={makeNetworkAsOnWifi()}>
+        {children}
+      </Network.Provider>
     );
 
     const { result, waitForNextUpdate } = renderHook(() => useIsAtHome(), {
@@ -29,13 +29,10 @@ describe("useIsAtHome", () => {
   });
 
   test("is not at home when not on wifi", async () => {
-    const network = {
-      async getNetworkStateAsync(): Promise<any> {
-        return {};
-      },
-    };
-    const wrapper = ({ children }: any) => (
-      <Network.Provider value={network}>{children}</Network.Provider>
+    const wrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => (
+      <Network.Provider value={makeNetworkAsNotConnected()}>
+        {children}
+      </Network.Provider>
     );
 
     const { result, waitForNextUpdate } = renderHook(() => useIsAtHome(), {
@@ -47,13 +44,10 @@ describe("useIsAtHome", () => {
   });
 
   test("the result future isn't ready if the call fails", async () => {
-    const network = {
-      async getNetworkStateAsync(): Promise<any> {
-        throw new Error("expected error");
-      },
-    };
-    const wrapper = ({ children }: any) => (
-      <Network.Provider value={network}>{children}</Network.Provider>
+    const wrapper = ({ children }: PropsWithChildren<unknown>): JSX.Element => (
+      <Network.Provider value={makeNetworkAsAFailure()}>
+        {children}
+      </Network.Provider>
     );
 
     const { result, waitForNextUpdate } = renderHook(() => useIsAtHome(), {
