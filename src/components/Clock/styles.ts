c9eecbb41b963684c312846b3701a5e987a4e553
Vladimir Iakovlev <nvbn.rm@gmail.com>
2021-01-24 16:45:50 +0100

    #5: Show the whole clock circle as green when the curfew is over

--- src/components/Clock/styles.ts
+++ src/components/Clock/styles.ts
@@ -1,5 +1,35 @@
 import { StyleSheet } from "react-native";
 
+export const CLOCK_WIDTH = "100%";
+
+export const CLOCK_HEIGHT = "100%";
+
+export const CLOCK_VIEWBOX = "0 0 100 100";
+
+export const CLOCK_CENTER_X = 50;
+
+export const CLOCK_CENTER_Y = 50;
+
+export const CLOCK_MESSAGE_Y = 60;
+
+export const CLOCK_MESSAGE_FONT_SIZE = 6;
+
+export const CLOCK_FINE_CIRCLE_RADIUS = 45;
+
+export const CLOCK_EMPTY_CIRCLE_RADIUS = 40;
+
+export const CLOCK_WARNING_ARC_RADIUS = 40;
+
+export const CLOCK_WARNING_ARC_WIDTH = 10;
+
+export const CLOCK_CURRENT_TIME_ARC_RADIUS = 42.5;
+
+export const CLOCK_CURRENT_TIME_ARC_WIDTH = 10;
+
+export const CLOCK_CURRENT_TIME_TEXT_ANCHOR = "middle";
+
+export const CLOCK_CURRENT_TIME_FONT_SIZE = 16;
+
 export const COLOR_OK = "#33DE7C";
 
 export const COLOR_DANGER = "#DEC028";
@@ -9,6 +39,8 @@ export const COLOR_CURFEW = "#DE5F54";
 export const COLOR_CURRENT = "#3128DE";
 
 export const COLOR_EMPTY = "#ffffff";
+
+export const COLOR_TEXT = "#000000";
 
 export default StyleSheet.create({
   container: {
