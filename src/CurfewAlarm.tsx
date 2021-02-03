5c38e50f0f383622de5a2ec67266f202c59f7648
Vladimir Iakovlev <nvbn.rm@gmail.com>
2021-02-03 21:09:50 +0100

    #13: Move background tasks initialisation to initialisers

--- src/CurfewAlarm.tsx
+++ src/CurfewAlarm.tsx
@@ -1,48 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import * as BackgroundFetch from "expo-background-fetch";
-import * as TaskManager from "expo-task-manager";
 import React from "react";
 
 import SettingsButton from "./components/SettingsButton";
-import { ConstantsDefaultImpl } from "./dependencies/Constants";
-import { DateTimeDefaultImpl } from "./dependencies/DateTime";
-import { NetworkDefaultImpl } from "./dependencies/Network";
-import { NotificationSenderDefaultImpl } from "./dependencies/Notifications";
-import { PersistentStorageDefaultImpl } from "./dependencies/PersistentStorage";
 import Dependencies from "./initialisers/Dependencies";
 import initI18n from "./initialisers/initI18n";
 import initSentry from "./initialisers/initSentry";
+import setupBackgroundTasks from "./initialisers/setupBackgroundTasks";
 import setupExpoNotificationsHandler from "./initialisers/setupExpoNotificationHandler";
 import Home, { HOME_SCREEN_NAME } from "./screens/Home";
 import Settings, { SETTINGS_SCREEN_NAME } from "./screens/Settings";
-import notificationsSender, {
-  NOTIFICATIONS_TASK_NAME,
-} from "./tasks/notificationsSender";
 import i18n from "./utils/i18n";
 
 initSentry();
 initI18n();
 setupExpoNotificationsHandler();
-
-TaskManager.defineTask(NOTIFICATIONS_TASK_NAME, async () => {
-  notificationsSender(
-    ConstantsDefaultImpl,
-    DateTimeDefaultImpl,
-    PersistentStorageDefaultImpl,
-    NetworkDefaultImpl,
-    NotificationSenderDefaultImpl,
-  ).catch((e) => console.warn("background task failed", e));
-
-  return BackgroundFetch.Result.NoData;
-});
-
-BackgroundFetch.registerTaskAsync(NOTIFICATIONS_TASK_NAME, {
-  startOnBoot: true,
-  minimumInterval: 300,
-}).catch((e) =>
-  console.warn("unable to register notifications background task", e),
-);
+setupBackgroundTasks();
 
 const Stack = createStackNavigator();
 
